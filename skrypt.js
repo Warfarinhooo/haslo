1b9b70af57857ffb785082e66528ff7cdb3683e9
Warfarinhooo <97955884+Warfarinhooo@users.noreply.github.com>
2022-09-14 15:52:12 +0200

    Add files via upload

---
+++ skrypt.js
@@ -0,0 +1,8 @@
+function wybieranieDuzeL(){
+    var iloscDuzeL=document.getElementById("iloscDuzeL").value;
+    document.getElementById("wybDuzeL").value=iloscDuzeL;
+}
+function wybieranieMaleL(){
+    var iloscDuzeL=document.getElementById("iloscMaleL").value;
+    document.getElementById("wybMaleL").value=iloscDuzeL;
+}
