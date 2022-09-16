3c8fd629ae99850b49f97d1290d9792b08568fea
Warfarinhooo <97955884+Warfarinhooo@users.noreply.github.com>
2022-09-16 11:20:27 +0200

    Add files via upload

--- skrypt.js
+++ skrypt.js
@@ -1,8 +1,25 @@
+var iloscZn=8;
+var iloscWolnychZnakow=0;
+var iloscDuzeL=0;
+var iloscMaleL=0;
+function ustawWolne(){
+    iloscWolnychZnakow=+iloscZn-(+iloscDuzeL+ +iloscMaleL);
+    document.getElementById("iloscDuzeL").max=+iloscWolnychZnakow+ +iloscDuzeL;
+    document.getElementById("iloscMaleL").max=+iloscWolnychZnakow+ +iloscMaleL;
+}
 function wybieranieDuzeL(){
-    var iloscDuzeL=document.getElementById("iloscDuzeL").value;
+    iloscDuzeL=document.getElementById("iloscDuzeL").value;
     document.getElementById("wybDuzeL").value=iloscDuzeL;
+    ustawWolne()
 }
 function wybieranieMaleL(){
-    var iloscDuzeL=document.getElementById("iloscMaleL").value;
-    document.getElementById("wybMaleL").value=iloscDuzeL;
+    iloscMaleL=document.getElementById("iloscMaleL").value;
+    document.getElementById("wybMaleL").value=iloscMaleL;
+    ustawWolne()
+}
+function iloscZnakow(){
+    iloscZn=document.getElementById("iloscZnakow").value;
+    ustawWolne()
+
+
 }
